595a308ee7aedb933c6081f2d28378abca5af512
Peter Banh <peter.banh@my.utsa.edu>
2022-02-22 14:53:38 -0600

    New Feature changes

--- EgitTest/src/HelloWorld.java
+++ EgitTest/src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Testing");
 		System.out.println("Refresh if needed");
-
+		System.out.println("New features made here");
 	}
 
 }
